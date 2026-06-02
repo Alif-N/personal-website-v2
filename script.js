b43086170560a214c1cd5b0ea54f68fc5978d0ce
Alif-N <alifnasrullah05@gmail.com>
2026-06-02 22:37:23 +0700

    add contact, journey section and refactor design

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-document.addEventListener("scroll", function () {
+document.addEventListener("DOMContentLoaded", function () {
     const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
 
-document.addEventListener("scroll", function () {
-    const marquee = document.querySelector(".marquee");
-    const scrollPosition = window.scrollY; // Posisi scroll saat ini
-    const speed = 0.3; // Kecepatan teks berjalan
-    marquee.style.transform = `translateX(${100 - scrollPosition * speed}%)`;
+    function toggleNavbarScroll() {
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    }
+
+    // Jalankan saat halaman di-scroll
+    window.addEventListener("scroll", toggleNavbarScroll);
+
+    // Jalankan sekali saat load halaman untuk memastikan kondisi scroll awal benar
+    toggleNavbarScroll();
 });
